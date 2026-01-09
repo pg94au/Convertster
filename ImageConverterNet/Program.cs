2ae30f89c1b55fdc24cc1e42213bd28ceb4ea761
SINISTER\paul <paul@grebenc.ca>
2026-01-08 21:44:06 -0500

    Add .net 4.8 image converter application.

---
+++ ImageConverterNet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ImageConverterNet
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var debugOutputFile = Environment.GetEnvironmentVariable("BLINKENLIGHTS_IMAGE_CONVERTER_DEBUG_OUTPUT");
+            if (debugOutputFile != null)
+            {
+                Trace.Listeners.Add(new TextWriterTraceListener(debugOutputFile));
+                Trace.AutoFlush = true;
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ProgressForm(args[0], args.Skip(1).ToArray()));
+        }
+    }
+}
---
+++ ImageConverterNet/ProgressForm.cs
@@ -0,0 +1,90 @@
+﻿using NetVips;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageConverterNet
+{
+    public partial class ProgressForm : Form
+    {
+        private readonly string _targetType;
+        private readonly string[] _filenames;
+
+        public ProgressForm(string targetType, string[] filenames)
+        {
+            InitializeComponent();
+
+            convertingFilesLabel.Text = $"Converting files to {targetType}...";
+
+            _targetType = targetType;
+            Trace.WriteLine($"Target type is {_targetType}");
+            _filenames = filenames;
+            Trace.WriteLine($"Filenames to convert: {string.Join(", ", _filenames)}");
+        }
+
+        protected override async void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            conversionProgressBar.Minimum = 0;
+            conversionProgressBar.Maximum = _filenames.Length;
+            Trace.WriteLine($"Progress bar maximum = {conversionProgressBar.Maximum}");
+
+            // Start the conversion process here or in a separate method
+            var anySuccess = false;
+            foreach (var filename in _filenames)
+            {
+                // Update the label to show the current file being processed
+                currentFileNameLabel.Text = filename;
+
+                await Task.Yield();
+
+                try
+                {
+                    ConvertImageType(_targetType, filename);
+                    Trace.WriteLine($"Successfully converted {filename}");
+                    anySuccess = true;
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine($"Error converting {filename}: {ex.Message}");
+                    conversionProgressBar.SetState(ProgressBarExtensions.ProgressBarState.Paused);
+                }
+
+                conversionProgressBar.Value += 1;
+                //conversionProgressBar.Refresh();
+                await Task.Delay(1);
+
+                await Task.Yield();
+            }
+
+            if (!anySuccess)
+            {
+                conversionProgressBar.SetState(ProgressBarExtensions.ProgressBarState.Error);
+            }
+
+            currentFileNameLabel.Text = "Conversion complete!";
+            Trace.WriteLine($"Progress bar value = {conversionProgressBar.Value}");
+        }
+
+        private void ConvertImageType(string targetType, string filename)
+        {
+            var image = Image.NewFromFile(filename, access: Enums.Access.Sequential);
+
+            switch (targetType)
+            {
+                case "JPG":
+                    var jpgPath = Path.ChangeExtension(filename, ".jpg");
+                    // TODO: Have some way to specify quality.
+                    image.Jpegsave(jpgPath, q: 75);
+                    break;
+                case "PNG":
+                    var pngPath = Path.ChangeExtension(filename, ".png");
+                    image.Pngsave(pngPath);
+                    break;
+            }
+        }
+    }
+}
