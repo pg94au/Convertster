68e9301ec12a22ddcba938980c7dc7516d7dfde6
SINISTER\paul <paul@grebenc.ca>
2026-01-10 12:20:51 -0500

    Switch to ImageSharp.

--- ImageConverterNet/ProgressForm.cs
+++ ImageConverterNet/ProgressForm.cs
@@ -1,4 +1,6 @@
-﻿using NetVips;
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
 using System;
 using System.Diagnostics;
 using System.IO;
@@ -43,7 +45,7 @@ namespace ImageConverterNet
 
                 try
                 {
-                    ConvertImageType(_targetType, filename);
+                    await ConvertImageType(_targetType, filename);
                     Trace.WriteLine($"Successfully converted {filename}");
                     anySuccess = true;
                 }
@@ -69,20 +71,21 @@ namespace ImageConverterNet
             Trace.WriteLine($"Progress bar value = {conversionProgressBar.Value}");
         }
 
-        private void ConvertImageType(string targetType, string filename)
+        private async Task ConvertImageType(string targetType, string filename)
         {
-            var image = Image.NewFromFile(filename, access: Enums.Access.Sequential);
+            var image = await SixLabors.ImageSharp.Image.LoadAsync(filename);
 
             switch (targetType)
             {
                 case "JPG":
                     var jpgPath = Path.ChangeExtension(filename, ".jpg");
                     // TODO: Have some way to specify quality.
-                    image.Jpegsave(jpgPath, q: 75);
+                    await image.SaveAsJpegAsync(jpgPath, new JpegEncoder { Quality = 75 });
                     break;
                 case "PNG":
                     var pngPath = Path.ChangeExtension(filename, ".png");
-                    image.Pngsave(pngPath);
+                    await image.SaveAsPngAsync(pngPath,
+                        new PngEncoder { CompressionLevel = PngCompressionLevel.BestCompression });
                     break;
             }
         }
