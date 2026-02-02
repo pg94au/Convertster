98b50e24daa2f537d06ccb8676ec977a07474057
SINISTER\paul <paul@grebenc.ca>
2026-02-01 22:08:56 -0500

    Remove some missed references to old namespace.

--- ImageConverter/Properties/Settings.Designer.cs
+++ ImageConverter/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ImageConverterNet.Properties
+namespace ImageConverter.Properties
 {
 
 
